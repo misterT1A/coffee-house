18bba1cacef38f272c095d2fdad1fbcc5edc2c16
misterT1A <paradogss@mail.ru>
2023-11-20 21:34:11 +0300

    fix: logoLink, h1 menu page

--- coffee-house/src/index.js
+++ coffee-house/src/index.js
@@ -9,6 +9,11 @@ import footer from './modules/footer/footer';
 const bodyMain = document.querySelector('body');
 bodyMain.append(header.header, main, footer.footer);
 
+const logoLink = document.querySelector('.header_main_page');
+// eslint-disable-next-line no-script-url
+logoLink.setAttribute('href', 'javascript:void(0)');
+logoLink.classList.add('menu_logo_disabled');
+
 const mainPageBtn = document.querySelector('.header_main_page');
 const menuBtn = document.querySelector('.header_btn');
 
