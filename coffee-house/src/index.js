a3ec2461b030173d35b523fe66f4cc8a55fe7f82
misterT1A <paradogss@mail.ru>
2023-11-21 19:57:35 +0300

    feat: add burger menu

--- coffee-house/src/index.js
+++ coffee-house/src/index.js
@@ -20,3 +20,28 @@ const menuBtn = document.querySelector('.header_btn');
 menuBtn.addEventListener('click', () => {
   menuBtn.classList.add('active');
 });
+
+const nav = document.querySelector('.nav_wrapper');
+const burgerBtn = document.querySelector('.burger_btn');
+const { body } = document;
+
+burgerBtn.addEventListener('click', () => {
+  if (!nav.classList.contains('nav_active')) {
+    // bodyMain.classList.add('body_overflow_hidden');
+    burgerBtn.classList.add('burger_btn_active');
+    nav.setAttribute('style', 'display: flex;');
+
+    setTimeout(() => {
+      body.setAttribute('style', 'overflow: hidden');
+      nav.classList.add('nav_active');
+    }, 100);
+  } else {
+    // bodyMain.classList.remove('body_overflow_hidden');
+    burgerBtn.classList.remove('burger_btn_active');
+    nav.classList.remove('nav_active');
+    setTimeout(() => {
+      nav.removeAttribute('style', 'display: flex;');
+      body.removeAttribute('style', 'overflow: hidden');
+    }, 300);
+  }
+});
